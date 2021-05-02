0559d38c2461e7b9cdcb24f2e0e8803757da474e
Otavio Miguel <otaviomiguel19@gmail.com>
2021-05-01 23:52:37 -0300

    Commit para tu

---
+++ comunicacao_dados/lib/routes/RoutesConfigs.dart
@@ -0,0 +1,11 @@
+import 'package:comunicacao_dados/routes/AppRoutesEnum.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RoutesConfigs {
+  static String calculateRoute() {
+    if (FirebaseAuth.instance.currentUser == null || FirebaseAuth.instance.currentUser.uid == null || FirebaseAuth.instance.currentUser.uid.isEmpty) {
+      return AppRoutesEnum.login;
+    }
+    return '';
+  } 
+}
